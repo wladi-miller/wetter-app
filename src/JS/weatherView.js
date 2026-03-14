1513f34ec124936b0488167d1be03b1f68a57fd9
Wladimir Miller <wladim.miller@gmail.com>
2026-03-14 18:38:36 +0100

    add condition.js

--- src/JS/weatherView.js
+++ src/JS/weatherView.js
@@ -1,7 +1,8 @@
 import { getWeatherForecast } from "./api.js";
 import { rootApp } from "../main.js";
 import { showLoading } from "./loading.js";
-import { getDetailViewHtml } from "./JS_Weather_View/detailView.js";
+import { getConditionImagePath } from "../conditions.js";
+import { getDetailViewHtml } from "./JS_Weather_View/headerView.js";
 import { getForecast24hViewHtml } from "./JS_Weather_View/forecast24hView.js";
 import { getForecast3dViewHtml } from "./JS_Weather_View/forecast3dView.js";
 import { getFurtherInfoViewHtml } from "./JS_Weather_View/furtherInfoView.js";
